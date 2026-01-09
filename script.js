060b6805dce92add4c8a8adef582045e2d8b621d
Amen Sime <amensime@gmail.com>
2026-01-09 23:41:21 +0300

    onclick on button and The rendering function

--- script.js
+++ script.js
@@ -3,6 +3,31 @@
     function saveTodos() {
         localStorage.setItem("todos", JSON.stringify(todos));
     }
+
+
+function renderTodos() {
+    const list = document.getElementById("myList");
+    list.innerHTML = "";
+
+        todos.forEach((todo, index) => {
+            const li = document.createElement("li");
+            li.className = todo.completed ? "completed" : "";
+
+            const span = document.createElement("span");
+            span.textContent = todo.text;
+            span.onclick = () => toggleComplete(index);
+
+            const del = document.createElement("span");
+            del.textContent = "DELETE";
+            del.className = "delete";
+            del.onclick = () => deleteTodo(index);
+
+            li.appendChild(span);
+            li.appendChild(del);
+            list.appendChild(li);
+        });
+    }
+
 function addTodo() {
         const input = document.getElementById("Input");
         if (input.value.trim() === "") return;
@@ -10,16 +35,19 @@ function addTodo() {
         todos.push({ text: input.value, completed: false });
         input.value = "";
         saveTodos();
-        //renderTodos();
+        renderTodos();
     }
       function toggleComplete(index) {
         todos[index].completed = !todos[index].completed;
         saveTodos();
-        //renderTodos();
+         renderTodos();
     }
 
     function deleteTodo(index) {
         todos.splice(index, 1);
         saveTodos();
-        //renderTodos();
+        renderTodos();
     }
+
+    renderTodos();
+
