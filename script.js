ede129df0745dc5031ade45057f908fdb2e0ca15
Amen Sime <amensime@gmail.com>
2026-01-09 23:25:35 +0300

    the add function

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
     function saveTodos() {
         localStorage.setItem("todos", JSON.stringify(todos));
     }
+function addTodo() {
+        const input = document.getElementById("Input");
+        if (input.value.trim() === "") return;
+
+        todos.push({ text: input.value, completed: false });
+        input.value = "";
+        saveTodos();
+        renderTodos();
+    }
